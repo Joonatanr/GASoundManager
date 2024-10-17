210b6208d7ab6e48274d8e2a7556ed3d90b43f5c
Joonatanr <joonatanrenel@gmail.com>
2024-10-17 14:01:47 +0300

    Finished adding basic audio player functionality.

--- GASoundManagerApp/GASoundManagerApp/Form1.Designer.cs
+++ GASoundManagerApp/GASoundManagerApp/Form1.Designer.cs
@@ -31,15 +31,20 @@
             components = new System.ComponentModel.Container();
             buttonPlay = new Button();
             groupBox1 = new GroupBox();
+            labelTrackName = new Label();
             labelPosition = new Label();
             trackBar1 = new TrackBar();
             groupBox2 = new GroupBox();
+            button4 = new Button();
+            trackBarVolume = new TrackBar();
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
             timer1 = new System.Windows.Forms.Timer(components);
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarVolume).BeginInit();
             SuspendLayout();
             // 
             // buttonPlay
@@ -54,39 +59,86 @@
             // 
             // groupBox1
             // 
+            groupBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.Controls.Add(labelTrackName);
             groupBox1.Controls.Add(labelPosition);
             groupBox1.Controls.Add(trackBar1);
-            groupBox1.Location = new Point(12, 292);
+            groupBox1.Location = new Point(12, 294);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(548, 130);
+            groupBox1.Size = new Size(548, 144);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             groupBox1.Text = "Track";
             // 
+            // labelTrackName
+            // 
+            labelTrackName.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            labelTrackName.AutoSize = true;
+            labelTrackName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
+            labelTrackName.Location = new Point(15, 23);
+            labelTrackName.Name = "labelTrackName";
+            labelTrackName.Size = new Size(146, 21);
+            labelTrackName.TabIndex = 2;
+            labelTrackName.Text = "<No track playing>";
+            // 
             // labelPosition
             // 
             labelPosition.AutoSize = true;
-            labelPosition.Location = new Point(17, 38);
+            labelPosition.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 186);
+            labelPosition.Location = new Point(16, 50);
             labelPosition.Name = "labelPosition";
-            labelPosition.Size = new Size(32, 15);
+            labelPosition.Size = new Size(52, 25);
             labelPosition.TabIndex = 1;
             labelPosition.Text = "--/--";
             // 
             // trackBar1
             // 
-            trackBar1.Location = new Point(6, 79);
+            trackBar1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            trackBar1.Location = new Point(6, 83);
             trackBar1.Name = "trackBar1";
             trackBar1.Size = new Size(536, 45);
             trackBar1.TabIndex = 0;
+            trackBar1.Scroll += trackBar1_Scroll;
+            trackBar1.MouseDown += trackBar1_MouseDown;
+            trackBar1.MouseUp += trackBar1_MouseUp;
             // 
             // groupBox2
             // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            groupBox2.Controls.Add(button4);
+            groupBox2.Controls.Add(trackBarVolume);
             groupBox2.Location = new Point(566, 12);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(222, 410);
+            groupBox2.Size = new Size(222, 426);
             groupBox2.TabIndex = 0;
             groupBox2.TabStop = false;
             groupBox2.Text = "Volume";
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 186);
+            button4.ForeColor = Color.Red;
+            button4.Location = new Point(76, 334);
+            button4.Name = "button4";
+            button4.Size = new Size(140, 86);
+            button4.TabIndex = 1;
+            button4.Text = "KILL VOLUME";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // trackBarVolume
+            // 
+            trackBarVolume.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            trackBarVolume.Location = new Point(25, 22);
+            trackBarVolume.Maximum = 100;
+            trackBarVolume.Name = "trackBarVolume";
+            trackBarVolume.Orientation = Orientation.Vertical;
+            trackBarVolume.Size = new Size(45, 398);
+            trackBarVolume.TabIndex = 0;
+            trackBarVolume.TickFrequency = 2;
+            trackBarVolume.Value = 100;
+            trackBarVolume.ValueChanged += trackBar2_ValueChanged;
             // 
             // button1
             // 
@@ -133,11 +185,15 @@
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Controls.Add(buttonPlay);
+            MinimumSize = new Size(420, 420);
             Name = "Form1";
             Text = "GA Estonian Evening Audio Player";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarVolume).EndInit();
             ResumeLayout(false);
         }
 
@@ -152,5 +208,8 @@
         private TrackBar trackBar1;
         private Label labelPosition;
         private System.Windows.Forms.Timer timer1;
+        private Button button4;
+        private TrackBar trackBarVolume;
+        private Label labelTrackName;
     }
 }
--- GASoundManagerApp/GASoundManagerApp/Form1.cs
+++ GASoundManagerApp/GASoundManagerApp/Form1.cs
@@ -4,47 +4,108 @@ namespace GASoundManagerApp
 {
     public partial class Form1 : Form
     {
-        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        private WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
 
+        private bool isUserUpdatingTrackbar = false;
 
         public Form1()
         {
             InitializeComponent();
             timer1.Start();
+
+            wplayer.MediaChange += Wplayer_MediaChange;
+        }
+
+        private void Wplayer_MediaChange(object Item)
+        {
+            trackBar1.Maximum = (int)wplayer.currentMedia.duration;
+            labelTrackName.Text = wplayer.currentMedia.name;
+        }
+
+        private void playAudio(string path)
+        {
+            try
+            {
+                wplayer.controls.stop();
+                wplayer.URL = path;
+                wplayer.controls.play();
+            }
+            catch (Exception ex)
+            {
+                /* TODO : Report error. */
+            }
+        }
+
+        private void setPositionString(double currLoc, double duration)
+        {
+            var curr = TimeSpan.FromSeconds(currLoc);
+            var dur = TimeSpan.FromSeconds(duration);
+
+            labelPosition.Text = curr.ToString(@"mm\:ss") + " / " + dur.ToString(@"mm\:ss");
         }
 
         private void buttonPlay_Click(object sender, EventArgs e)
         {
-            wplayer.URL = "audio_clips/Test/MountainKing_edited2.mp3";
-            wplayer.controls.play();
+            playAudio("audio_clips/Test/MountainKing_edited2.mp3");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            wplayer.URL = "audio_clips/Test/MorningMood_edited.mp3";
-            wplayer.controls.play();
+            playAudio("audio_clips/Test/MorningMood_edited.mp3");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            wplayer.URL = "audio_clips/Test/whip.mp3";
-            wplayer.controls.play();
+            playAudio("audio_clips/Test/whip.mp3");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            wplayer.URL = "audio_clips/Test/whip_x2.mp4";
-            wplayer.controls.play();
+            playAudio("audio_clips/Test/whip_x2.mp4");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (wplayer.currentMedia != null)
             {
-                labelPosition.Text = wplayer.controls.currentPositionString + "/" + wplayer.currentMedia.durationString;
-            }
+                setPositionString(wplayer.controls.currentPosition, wplayer.currentMedia.duration);
 
-            trackBar1.Value = (int)wplayer.controls.currentPosition;
+                if (!isUserUpdatingTrackbar)
+                {
+                    trackBar1.Value = (int)wplayer.controls.currentPosition;
+                }
+            }
+        }
+
+
+        private void trackBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isUserUpdatingTrackbar = true;
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isUserUpdatingTrackbar = false;
+
+            if (wplayer.currentMedia != null)
+            {
+                wplayer.controls.currentPosition = trackBar1.Value;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            wplayer.settings.volume = trackBarVolume.Value;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            trackBarVolume.Value = 0;
         }
     }
 }
